9103a572db82134951a21f60f2265d805c12fab9
AzureAD\AustenHarp <austen.harp@reprivata.com>
2021-02-02 13:03:55 -0600

    Renamed class name to match CoT Connects calls

---
+++ cotstrongswan/src/main/java/org/strongswan/android/logic/VpnBroadcastReceiver.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 Reprivata
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+package org.strongswan.android.logic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.strongswan.android.logic.CharonVpnService;
+
+public class VpnBroadcastReceiver extends BroadcastReceiver {
+
+    static final String ACTION_DISCONNECT = "OK";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (ACTION_DISCONNECT.equals(intent.getAction())) {
+
+            Intent i = new Intent(context, CharonVpnService.class);
+            i.setAction(CharonVpnService.DISCONNECT_ACTION);
+            context.startService(i);
+        }
+    }
+}
